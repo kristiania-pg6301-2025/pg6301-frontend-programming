b6dfac364a0acacd0a6a70bf8730619f59874cb1
Johannes Brodwall <jhannes@gmail.com>
2025-08-14 17:37:15 +0200

    🔴 2032 is a leap year

--- leapYears.test.js
+++ leapYears.test.js
@@ -10,3 +10,6 @@ test("2025 is not a leap year", () => {
 test("2028 is a leap year", () => {
   expect(isLeapYear(2028)).toBe(true);
 });
+test("2032 is a leap year", () => {
+  expect(isLeapYear(2032)).toBe(true);
+});
