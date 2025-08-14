6fdc50805192d06bd17d7e621e9de5d7a6881558
Johannes Brodwall <jhannes@gmail.com>
2025-08-14 17:38:47 +0200

    🔴 2100 is not a leap year

--- leapYears.test.js
+++ leapYears.test.js
@@ -13,3 +13,6 @@ test("2028 is a leap year", () => {
 test("2032 is a leap year", () => {
   expect(isLeapYear(2032)).toBe(true);
 });
+test("2100 is a not leap year", () => {
+  expect(isLeapYear(2100)).toBe(false);
+});
