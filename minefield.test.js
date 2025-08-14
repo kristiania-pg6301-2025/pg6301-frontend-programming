5a722428591f7d9abe33cbf939eba5d4422d5526
Johannes Brodwall <jhannes@gmail.com>
2025-08-14 17:55:17 +0200

    🔴 should display hint to the left

--- minefield.test.js
+++ minefield.test.js
@@ -38,3 +38,4 @@ testMinefield(
 );
 testMinefield("should display mines", ["****", "****"], ["****", "****"]);
 testMinefield("should display hint to the right", ["*."], ["*1"]);
+testMinefield("should display hint to the left", [".*."], ["1*1"]);
