6ded07cdcf610a84ff605518b566690b33f543ae
Johannes Brodwall <jhannes@gmail.com>
2025-08-14 17:34:58 +0200

    🔴 Generate `isLeapYear` function

--- leapYears.test.js
+++ leapYears.test.js
@@ -1,5 +1,9 @@
 import { test, expect } from "vitest";
 
+function isLeapYear(number) {
+  return undefined;
+}
+
 test("2025 is not a leap year", () => {
   expect(isLeapYear(2025)).toBe(false);
 });
