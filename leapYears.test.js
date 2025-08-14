9facd133b8c8bfbeaf9e9269ea58e5427e027ec6
Johannes Brodwall <jhannes@gmail.com>
2025-08-14 17:36:15 +0200

    🔴 failing test for 2028

--- leapYears.test.js
+++ leapYears.test.js
@@ -7,3 +7,6 @@ function isLeapYear(number) {
 test("2025 is not a leap year", () => {
   expect(isLeapYear(2025)).toBe(false);
 });
+test("2028 is a leap year", () => {
+  expect(isLeapYear(2028)).toBe(true);
+});
