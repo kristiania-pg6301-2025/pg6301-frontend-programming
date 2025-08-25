141557f41c26ba3eb0dc71a1d5ffad60dfb67e0e
Johannes Brodwall <jhannes@gmail.com>
2025-08-25 18:24:45 +0200

    🔴 test: "1 in roman numbers is I"

---
+++ romanNumerals.test.js
@@ -0,0 +1,9 @@
+import { test, expect } from "vitest";
+
+function romanNumbers(number) {
+  return undefined;
+}
+
+test("1 in roman numbers is I", () => {
+  expect(romanNumbers(1)).toBe("I");
+});
