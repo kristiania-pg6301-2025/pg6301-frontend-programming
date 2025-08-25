93ea3b113dd13be25852e23eddee07320368e251
Johannes Brodwall <jhannes@gmail.com>
2025-08-25 18:25:39 +0200

    🔴 test: "2 in roman numbers is II"

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -7,3 +7,7 @@ function romanNumbers(number) {
 test("1 in roman numbers is I", () => {
   expect(romanNumbers(1)).toBe("I");
 });
+
+test("2 in roman numbers is II", () => {
+  expect(romanNumbers(2)).toBe("II");
+});
