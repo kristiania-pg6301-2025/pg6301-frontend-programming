1117159393e0223a1285ff7d2ee3fc0cc8429ad0
Johannes Brodwall <jhannes@gmail.com>
2025-08-25 18:39:09 +0200

    🟢 "9 in roman numbers is IX"

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -2,6 +2,7 @@ import { test, expect } from "vitest";
 
 function romanNumbers(number) {
   let result = "";
+  if (number === 9) return "IX";
   if (number >= 5) {
     result += "V";
     number -= 5;
