2aa0aad7f9aeece60072453ad15c4c592f7df486
Johannes Brodwall <jhannes@gmail.com>
2025-08-25 18:59:32 +0200

    🔵 "555 in roman numbers is DLV"

--- romanNumerals.test.js
+++ romanNumerals.test.js
@@ -15,16 +15,9 @@ function romanNumbers(number) {
   convertDigits("L", 50);
   convertDigits("XL", 40);
   convertDigits("X", 10);
-
-  if (number === 9) return "IX";
-  if (number >= 5) {
-    result += "V";
-    number -= 5;
-  }
-  if (number >= 4) {
-    result += "IV";
-    number -= 4;
-  }
+  convertDigits("IX", 9);
+  convertDigits("V", 5);
+  convertDigits("IV", 4);
   convertDigits("I", 1);
   return result;
 }
@@ -38,13 +31,9 @@ function testRoman(number, expectedText) {
 testRoman(1, "I");
 testRoman(2, "II");
 testRoman(3, "III");
-testRoman(4, "IV");
-testRoman(5, "V");
 testRoman(6, "VI");
-testRoman(9, "IX");
 testRoman(10, "X");
 testRoman(11, "XI");
 testRoman(38, "XXXVIII");
-testRoman(40, "XL");
 testRoman(444, "CDXLIV");
 testRoman(555, "DLV");
