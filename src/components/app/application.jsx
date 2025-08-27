dc0f6a462f58928c40b1793b8c428b8428275d6d
Johannes Brodwall <johannes@brodwall.com>
2025-08-28 00:15:13 +0200

    Application overview

--- src/components/app/application.jsx
+++ src/components/app/application.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 
+function NewTaskForm() {
+  return (
+    <form>
+      <div>
+        <strong>Task description: </strong>
+        <input type="text" />
+      </div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+}
+
+function TaskList() {
+  return (
+    <ul>
+      <li>
+        <label>
+          <input type={"checkbox"} />
+          Task one
+        </label>
+      </li>
+    </ul>
+  );
+}
+
 export function Application() {
-  return <h1>Hello React</h1>;
+  return (
+    <>
+      <h1>Tasks</h1>
+      <h2>Create new task</h2>
+      <NewTaskForm />
+      <h2>Existing tasks</h2>
+      <TaskList />
+    </>
+  );
 }
