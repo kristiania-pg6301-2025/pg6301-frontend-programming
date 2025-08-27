7211191c6ccf8eddae170f227d88c54d5b5b8a5a
Johannes Brodwall <johannes@brodwall.com>
2025-08-27 22:51:58 +0200

    save new tasks on server

--- server/server.js
+++ server/server.js
@@ -2,17 +2,16 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
 const app = new Hono();
-app.get("/api/tasks", async (c) => {
-  return c.json([
-    { title: "Client side", completed: true },
-    { title: "Fetch from server", completed: true },
-    { title: "Post to server" },
-    { title: "Deployment" },
-  ]);
-});
+const tasks = [
+  { title: "Client side", completed: true },
+  { title: "Fetch from server", completed: true },
+  { title: "Post to server", completed: true },
+  { title: "Deployment" },
+];
+app.get("/api/tasks", async (c) => c.json(tasks));
 app.post("/api/tasks", async (c) => {
   const task = await c.req.json();
-  console.log(task);
+  tasks.push(task);
   return c.status(201);
 });
 serve(app);
