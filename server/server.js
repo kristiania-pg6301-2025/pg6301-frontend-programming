12858c0acbfe631180d79481f8f7494fd307995f
Johannes Brodwall <johannes@brodwall.com>
2025-08-27 22:42:42 +0200

    create GET /api/tasks with Hono
    ```
    app.get("/api/tasks", async (c) => {
      return c.json({ hello: "world" });
    });
    ```

--- server/server.js
+++ server/server.js
@@ -2,4 +2,12 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
 const app = new Hono();
+app.get("/api/tasks", async (c) => {
+  return c.json([
+    { title: "Client side", completed: true },
+    { title: "Fetch from server", completed: true },
+    { title: "Post to server" },
+    { title: "Deployment" },
+  ]);
+});
 serve(app);
