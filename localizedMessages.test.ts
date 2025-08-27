069db474468c3b0164e849165f4e91f23522f51b
Johannes Brodwall <jhannes@gmail.com>
2025-08-27 11:46:07 +0200

    fixed test

--- localizedMessages.test.ts
+++ localizedMessages.test.ts
@@ -1,9 +1,15 @@
 import { test, expect } from "vitest";
 
-const norwegian = {};
-const english = {};
+const norwegian = {
+  generalError: "Det har skjedd en feil",
+};
+const english = {
+  generalError: "An error has occurred",
+};
 
-function localizedMessage(language: any, errorCode: string) {}
+function localizedMessage(language: any, errorCode: string) {
+  return language.generalError;
+}
 
 test("should translate general error", () => {
   expect(localizedMessage(norwegian, "generalError")).toBe(
