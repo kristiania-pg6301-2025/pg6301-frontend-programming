3f11d9259be69d202ad3a40dfd53429e0269cc9b
Johannes Brodwall <johannes@brodwall.com>
2025-08-27 22:22:09 +0200

    React component structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("app")).render(<h1>Hello React</h1>);
+function Application() {
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  return (
+    <>
+      <h1>My tasks</h1>
+      <form>
+        <div>
+          Title: <input />
+        </div>
+        <div>
+          <button>Save task: New task</button>
+        </div>
+      </form>
+      <h2>Existing tasks</h2>
+    </>
+  );
+}
+
+createRoot(document.getElementById("app")).render(<Application />);
