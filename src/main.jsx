61d5f1c967ec937532a5756839689b82f2946e51
Johannes Brodwall <jhannes@gmail.com>
2025-09-10 10:45:40 +0200

    list tasks

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,25 @@
 import { createRoot } from "react-dom/client";
-import React from "react";
+import React, { useState } from "react";
 
-createRoot(document.getElementById("app")).render(<h1>React Application</h1>);
+function Application() {
+  const [tasks, setTasks] = useState([
+    { summary: "Create package.json file", completed: true },
+    { summary: "List existing tasks", completed: false },
+  ]);
+  return (
+    <>
+      <h1>Task manager</h1>
+      <h2>My tasks</h2>
+      {tasks.map((t) => (
+        <li>
+          <label>
+            <input type={"checkbox"} checked={t.completed} /> {t.summary}
+          </label>
+        </li>
+      ))}
+      <h2>New task</h2>
+    </>
+  );
+}
+
+createRoot(document.getElementById("app")).render(<Application />);
