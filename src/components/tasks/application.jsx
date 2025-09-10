fabce1531210a96fdb5bac69bbb53d9783322eae
Johannes Brodwall <jhannes@gmail.com>
2025-09-10 10:58:21 +0200

    NewTaskForm

--- src/components/tasks/application.jsx
+++ src/components/tasks/application.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import { TaskList } from "./taskList";
+import { NewTaskForm } from "./newTaskForm";
 
 export function Application() {
   const [tasks, setTasks] = useState([
     { summary: "Create package.json file", completed: true },
     { summary: "List existing tasks", completed: false },
   ]);
+  function handleNewTask(task) {
+    setTasks((old) => [...old, task]);
+  }
   return (
     <>
       <h1>Task manager</h1>
       <h2>My tasks</h2>
       <TaskList tasks={tasks} />
       <h2>New task</h2>
+      <NewTaskForm onNewTask={handleNewTask} />
     </>
   );
 }
