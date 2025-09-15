773825995bd8d107d53aeee593ef0378fa8856a0
Johannes Brodwall <jhannes@gmail.com>
2025-09-15 21:32:36 +0200

    Introduce `<TaskView />`

--- src/components/tasks/taskRoute.tsx
+++ src/components/tasks/taskRoute.tsx
@@ -1,9 +1,22 @@
 import type { TaskItem } from "../../taskItem.js";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+function TaskView({ task: { summary, completed } }: { task: TaskItem }) {
+  return (
+    <>
+      <h1>Single task: {summary}</h1>
+      <p>
+        <Link to={"/"}>All tasks</Link>
+      </p>
+      {completed && <p>Completed</p>}
+    </>
+  );
+}
 
 export function TaskRoute({ tasks }: { tasks: TaskItem[] }) {
   const { id } = useParams();
   const task = tasks.find((t) => t.id === parseInt(id!));
-  return <h1>Single task: {task?.summary}</h1>;
+  if (!task) return <h1>Task {id} not found</h1>;
+  return <TaskView task={task} />;
 }
