ef6d8f4bf2e3a8f852a7ae0061d41b140b52181c
Johannes Brodwall <jhannes@gmail.com>
2025-09-15 21:30:41 +0200

    show single task

--- src/components/tasks/taskRoute.tsx
+++ src/components/tasks/taskRoute.tsx
@@ -1,6 +1,9 @@
 import type { TaskItem } from "../../taskItem.js";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export function TaskRoute({ tasks }: { tasks: TaskItem[] }) {
-  return <h1>Single task</h1>;
+  const { id } = useParams();
+  const task = tasks.find((t) => t.id === parseInt(id!));
+  return <h1>Single task: {task?.summary}</h1>;
 }
