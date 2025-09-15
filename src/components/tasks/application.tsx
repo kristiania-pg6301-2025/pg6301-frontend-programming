cb595fb9e70d62a645487c9297622a258b4149e1
Johannes Brodwall <jhannes@gmail.com>
2025-09-15 21:39:25 +0200

    interlude: save tasks in localStorage

--- src/components/tasks/application.tsx
+++ src/components/tasks/application.tsx
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { TaskItem } from "../../taskItem.js";
 import { FrontPage } from "./frontPage.js";
 import { Route, Routes } from "react-router-dom";
 import { TaskRoute } from "./taskRoute.js";
 
+const initialTasks = [
+  { id: 0, summary: "Create package.json file", completed: true },
+  { id: 1, summary: "List existing tasks", completed: true },
+  { id: 2, summary: "Introduce typescript", completed: true },
+  { id: 3, summary: "Update state for checkboxes", completed: false },
+];
+
 export function Application() {
-  const [tasks, setTasks] = useState<TaskItem[]>([
-    { id: 0, summary: "Create package.json file", completed: true },
-    { id: 1, summary: "List existing tasks", completed: true },
-    { id: 2, summary: "Introduce typescript", completed: true },
-    { id: 3, summary: "Update state for checkboxes", completed: false },
-  ]);
+  const [tasks, setTasks] = useState<TaskItem[]>(() => {
+    const existingTasks = localStorage.getItem("tasks");
+    return existingTasks ? JSON.parse(existingTasks) : initialTasks;
+  });
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
   function handleNewTask(task: Omit<TaskItem, "id">) {
     setTasks((old) => [...old, { id: old.length, ...task }]);
   }
