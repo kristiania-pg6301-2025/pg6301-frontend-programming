28b7ade0eef2568955c82b0fe95f86a3991b5e83
Johannes Brodwall <johannes@brodwall.com>
2025-09-21 13:48:19 +0200

    move initialization of tasks to `useEffect`

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Application() {
-  const [tasks, setTasks] = useState([
-    { description: "Create project", completed: true },
-    { description: "Create React webapp", completed: false },
-    { description: "Create Hono backend", completed: false },
-  ]);
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    setTasks([
+      { description: "Create project", completed: true },
+      { description: "Create React webapp", completed: false },
+      { description: "Create Hono backend", completed: false },
+    ]);
+  }, []);
   return (
     <>
       <h1>My Task Manager</h1>
