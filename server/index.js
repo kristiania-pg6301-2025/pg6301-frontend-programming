cb6fc0383714c4f2884fee64a88cd7c585a79b73
Johannes Brodwall <johannes@brodwall.com>
2025-09-21 21:58:46 +0200

    handle POST /api/tasks on server

--- server/index.js
+++ server/index.js
@@ -12,5 +12,10 @@ const tasks = [
 app.get("/api/tasks", (c) => {
   return c.json(tasks);
 });
+app.post("/api/tasks", async (c) => {
+  const task = await c.req.json();
+  tasks.push(task);
+  return c.newResponse(null, 201);
+});
 
 serve(app);
