4f027ef8cd52f83e2b4a1be54b2c225788a5556d
Johannes Brodwall <johannes@brodwall.com>
2025-09-21 14:07:51 +0200

    implement server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
+const app = new Hono();
+
+const tasks = [
+  { description: "Create project (server)", completed: true },
+  { description: "Create React webapp (server)", completed: true },
+  { description: "Create Hono backend", completed: true },
+  { description: "Update with Hono backend", completed: false },
+];
+app.get("/api/tasks", (c) => {
+  return c.json(tasks);
+});
+
+serve(app);
