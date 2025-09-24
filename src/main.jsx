0e88e47bda6d3a2eb68913b64ebf808e8dac5b4a
Johannes Brodwall <jhannes@gmail.com>
2025-09-24 10:39:21 +0200

    show tasks on client

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 function Application() {
-  return <h1>Task Application</h1>;
+  const [tasks, setTasks] = useState([
+    { title: "Create React Application", completed: true },
+    { title: "Create Hono server", completed: false },
+    { title: "Create APIs", completed: false },
+  ]);
+  return (
+    <>
+      <h1>Task Application</h1>
+      <ul>
+        {tasks.map((t) => (
+          <li>{t.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")).render(<Application />);
