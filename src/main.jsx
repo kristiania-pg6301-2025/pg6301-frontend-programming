1f52dc82c27f78a3c768ecbdc9b3770479a25659
Johannes Brodwall <jhannes@gmail.com>
2025-09-24 10:43:19 +0200

    add new task

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,13 @@ function Application() {
     { title: "Create Hono server", completed: false },
     { title: "Create APIs", completed: false },
   ]);
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTasks((old) => [{ title, completed: false }, ...old]);
+  }
+
   return (
     <>
       <h1>Task Application</h1>
@@ -15,6 +22,13 @@ function Application() {
           <li>{t.title}</li>
         ))}
       </ul>
+      <h2>New task</h2>
+      <form onSubmit={handleSubmit}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <div>
+          <button>Add task</button>
+        </div>
+      </form>
     </>
   );
 }
