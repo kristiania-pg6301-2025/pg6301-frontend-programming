dcf5c10ad1d114fab694602a8b0acc35ceda0f3e
Johannes Brodwall <jhannes@gmail.com>
2025-09-28 15:04:26 +0200

    create view of tasks

--- src/application.tsx
+++ src/application.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface TaskItem {
+  description: string;
+  complete: boolean;
+}
+
+const initialTasks: TaskItem[] = [
+  { description: "Create client", complete: true },
+  { description: "Create server", complete: false },
+  { description: "Deploy server", complete: false },
+];
 
 export function Application() {
-  return <h1>Hello World</h1>;
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+  useEffect(() => {
+    setTasks(initialTasks);
+  }, []);
+  return (
+    <>
+      <h1>Task application</h1>
+      {tasks.map(({ description, complete }) => (
+        <li>
+          <input type="checkbox" checked={complete} /> {description}
+        </li>
+      ))}
+    </>
+  );
 }
