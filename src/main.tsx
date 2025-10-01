49919c7ff412ab28a3c13a3960ff6733958118d9
Johannes Brodwall <jhannes@gmail.com>
2025-10-01 10:30:39 +0200

    show task list

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,30 @@
 import { createRoot } from "react-dom/client";
+import { useEffect, useState } from "react";
+
+interface TaskItem {
+  description: string;
+  completed: boolean;
+}
+
+const taskList: TaskItem[] = [
+  { description: "Create react app", completed: true },
+  { description: "Create Hono application", completed: false },
+  { description: "Deploy to Heroku", completed: false },
+];
 
 function Application() {
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+  useEffect(() => {
+    setTasks(taskList);
+  }, []);
   return (
     <>
       <h1>My Non-deployed Task List</h1>
+      {tasks.map(({ description, completed }) => (
+        <li>
+          <input type="checkbox" checked={completed} /> {description}
+        </li>
+      ))}
     </>
   );
 }
