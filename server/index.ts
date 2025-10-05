03fd9cc06a6fd837c100f537c3b1e52d8b6b536d
Johannes Brodwall <jhannes@gmail.com>
2025-10-05 19:07:45 +0200

    improved error simulation

--- server/index.ts
+++ server/index.ts
@@ -16,19 +16,22 @@ const tasks = [
   { description: "Deal with errors from server", completed: false },
 ];
 
-async function delay(timeout: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
+async function delayWithError(timeout: number) {
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.2) {
+        reject(new Error("Error from promise"));
+      } else if (random < 0.3) {
+        reject(new HTTPException(400, { message: "Error from user" }));
+      } else {
+        resolve();
+      }
+    }, timeout);
   });
 }
 
 app.get("/api/tasks", async (c) => {
-  await delay(300);
-  if (Math.random() < 0.2) {
-    throw new Error("Server failure");
-  }
-  if (Math.random() < 0.4) {
-    throw new HTTPException(400, { message: "Invalid request" });
-  }
+  await delayWithError(300);
   return c.json(tasks);
 });
