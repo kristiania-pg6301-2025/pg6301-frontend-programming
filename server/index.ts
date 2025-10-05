fe70b450f491c9232a81ce47d878f0fc991244fb
Johannes Brodwall <jhannes@gmail.com>
2025-10-05 18:02:18 +0200

    Fetch data from server

--- server/index.ts
+++ server/index.ts
@@ -8,3 +8,11 @@ const port = process.env.PORT || "3000";
 serve({ fetch: app.fetch, port: parseInt(port) });
 
 app.get("*", serveStatic({ root: "../dist" }));
+
+const tasks = [
+  { description: "Fetch data from server", completed: true },
+  { description: "Deal with slow server", completed: false },
+  { description: "Deal with errors from server", completed: false },
+];
+
+app.get("/api/tasks", (c) => c.json(tasks));
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,30 @@
 import { createRoot } from "react-dom/client";
+import { useEffect, useState } from "react";
 
 function Application() {
-  return <h1>Hello World</h1>;
+  const [tasks, setTasks] = useState([
+    { description: "Fetch data from server", completed: false },
+  ]);
+
+  async function loadTasks() {
+    const res = await fetch("/api/tasks");
+    setTasks(await res.json());
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <h1>Tasks</h1>
+      {tasks.map(({ description, completed }) => (
+        <li>
+          <input type={"checkbox"} checked={completed} /> {description}
+        </li>
+      ))}
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
