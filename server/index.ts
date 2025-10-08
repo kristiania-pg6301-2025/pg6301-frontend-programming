2b8cd1ab1e2a2454bcfd4b841a08fc17c8364f03
Johannes Brodwall <jhannes@gmail.com>
2025-10-08 10:29:53 +0200

    Initial project

---
+++ server/index.ts
@@ -0,0 +1,10 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+import { serveStatic } from "@hono/node-server/serve-static";
+
+const app = new Hono();
+
+const port = process.env.PORT || "3000";
+serve({ fetch: app.fetch, port: parseInt(port) });
+
+app.get("*", serveStatic({ root: "../dist" }));
