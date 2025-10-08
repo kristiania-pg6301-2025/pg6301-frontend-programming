d3720dabef7649f2f268d846de47a9006a9e278e
Johannes Brodwall <jhannes@gmail.com>
2025-10-08 10:44:37 +0200

    fetch data from server

--- server/index.ts
+++ server/index.ts
@@ -7,4 +7,13 @@ const app = new Hono();
 const port = process.env.PORT || "3000";
 serve({ fetch: app.fetch, port: parseInt(port) });
 
+app.get("/api/tasks", (c) =>
+  c.json([
+    { description: "Deploy to heroku", complete: true },
+    { description: "Fetch data from server", complete: true },
+    { description: "Deal with slow requests", complete: false },
+    { description: "Deal with errors", complete: false },
+  ]),
+);
+
 app.get("*", serveStatic({ root: "../dist" }));
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 function Application() {
-  return <h1>Hello World</h1>;
+  const [tasks, setTasks] = useState([
+    { description: "Deploy to Heroku", completed: true },
+    { description: "Fetch tasks from server", completed: false },
+  ]);
+
+  async function loadTask() {
+    const res = await fetch("/api/tasks");
+    setTasks(await res.json());
+  }
+
+  useEffect(() => {
+    loadTask();
+  }, []);
+
+  return (
+    <>
+      <h1>My tasks</h1>
+      {tasks.map(({ description, completed }) => (
+        <li>
+          <input type={"checkbox"} checked={completed} /> {description}
+        </li>
+      ))}
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
