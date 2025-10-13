15eba4db128921f1ee2f81b240be1ada0849ea27
Johannes Brodwall <jhannes@gmail.com>
2025-10-13 18:26:31 +0200

    fetch tasks from server

--- server/index.ts
+++ server/index.ts
@@ -7,4 +7,12 @@ const app = new Hono();
 const port = process.env.PORT || "3000";
 serve({ fetch: app.fetch, port: parseInt(port) });
 
+app.get("/api/tasks", (c) =>
+  c.json([
+    { description: "Fetch tasks from server", completed: true },
+    { description: "Send tasks to server", completed: false },
+    { description: "Save tasks in database", completed: false },
+  ]),
+);
+
 app.get("*", serveStatic({ root: "../dist" }));
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,30 @@
 import { createRoot } from "react-dom/client";
+import { useEffect, useState } from "react";
+import type { TaskItem } from "../shared/taskItem.js";
 
 function Application() {
-  return <h1>Hello World</h1>;
+  const [tasks, setTasks] = useState<TaskItem[]>([]);
+
+  async function loadTasks() {
+    const res = await fetch("/api/tasks");
+    setTasks(await res.json());
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  return (
+    <>
+      <h1>My tasks</h1>
+      {tasks.map(({ description, completed }) => (
+        <li>
+          <input type="checkbox" checked={completed} />
+          {description}
+        </li>
+      ))}
+    </>
+  );
 }
 
 createRoot(document.getElementById("app")!).render(<Application />);
