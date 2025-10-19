a8847bd0c6a4c82e8779f9d054a8486a7dbc5e4c
Johannes Brodwall <jhannes@gmail.com>
2025-10-19 23:48:30 +0200

    handle authentication start

--- server/server.ts
+++ server/server.ts
@@ -4,8 +4,15 @@ import { serve } from "@hono/node-server";
 const app = new Hono();
 serve(app);
 
-app.get("/api/login/linkedin/start", (c) => {
-  return c.redirect(
+const client_id = "7792wb3of776if";
+
+app.get("/api/login/linkedin/start", async (c) => {
+  const res = await fetch(
     "https://www.linkedin.com/oauth/.well-known/openid-configuration",
+  );
+  const { authorization_endpoint } = await res.json();
+  const redirect_uri = "http://localhost:5173/api/login/linkedin/complete";
+  return c.redirect(
+    `${authorization_endpoint}?${new URLSearchParams({ client_id, redirect_uri, response_type: "code", scope: "email openid" })}`,
   );
 });
