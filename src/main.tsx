4080bcda4f9b98b0df2d107a8705d091914e4698
Johannes Brodwall <jhannes@gmail.com>
2025-10-19 20:10:56 +0200

    make client outline

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,53 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-function Application() {
-  return <h1>Hello, stranger</h1>;
+function FrontPage() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <ul>
+        <li>
+          <Link to={"/profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link to={"/login"}>Log in</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
-createRoot(document.getElementById("app")!).render(<Application />);
+function Profile() {
+  return <h1>User Profile</h1>;
+}
+
+function Login() {
+  return (
+    <>
+      <h1>Log in:</h1>
+      <ul>
+        <li>Login with LinkedIn</li>
+        <li>Login with Microsoft</li>
+        <li>Login with Google</li>
+      </ul>
+    </>
+  );
+}
+
+function Application() {
+  return (
+    <Routes>
+      <Route path={"/"} element={<FrontPage />} />
+      <Route path={"/profile"} element={<Profile />} />
+      <Route path={"/login"} element={<Login />} />
+      <Route path={"*"} element={<h1>Not found</h1>} />
+    </Routes>
+  );
+}
+
+createRoot(document.getElementById("app")!).render(
+  <BrowserRouter>
+    <Application />
+  </BrowserRouter>,
+);
