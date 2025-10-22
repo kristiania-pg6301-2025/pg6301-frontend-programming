7ee5ef8adcd983badc60a3f3008999b93b8836d4
Johannes Brodwall <jhannes@gmail.com>
2025-10-22 22:58:50 +0200

    authenticate with LinkedIn

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,90 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { getCookie, setCookie } from "hono/cookie";
 
 const app = new Hono();
 
 const port = process.env.PORT || "3000";
 serve({ fetch: app.fetch, port: parseInt(port) });
+
+const client_id = "77m8tju8g0vwaz";
+const client_secret = "WPL_AP1.GxRqFuQGTry0awzY.TVxm5w==";
+
+app.get("/api/login/:providerName/fetchToken", async (c) => {
+  const { providerName } = c.req.param();
+  const { error_description, code } = c.req.query();
+  if (error_description) return c.text(error_description);
+
+  if (!code) return c.text("Error: Code param is missing");
+
+  const { token_endpoint } = await getDiscoveryDoc();
+
+  const res = await fetch(token_endpoint, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      code,
+      client_id,
+      grant_type: "authorization_code",
+      client_secret,
+      redirect_uri: `http://localhost:5173/api/login/${providerName}/fetchToken`,
+    }),
+  });
+  const responseJson = await res.json();
+  if (false && "access_token" in responseJson) {
+    setCookie(c, "token", responseJson.access_token, {
+      httpOnly: true,
+      secure: true,
+    });
+    return c.redirect("/profile");
+  }
+
+  return c.json(responseJson);
+});
+
+async function getDiscoveryDoc() {
+  const res = await fetch(
+    "https://www.linkedin.com/oauth/.well-known/openid-configuration",
+  );
+
+  return await res.json();
+}
+
+app.get("/api/login/:providerName/start", async (c) => {
+  const providerName = c.req.param().providerName;
+
+  if (providerName === "linkedin") {
+    const { authorization_endpoint } = await getDiscoveryDoc();
+
+    const authorizationUri =
+      authorization_endpoint +
+      "?" +
+      new URLSearchParams({
+        client_id,
+        response_type: "code",
+        scope: "openid profile email",
+        redirect_uri: `http://localhost:5173/api/login/${providerName}/fetchToken`,
+      });
+
+    return c.redirect(authorizationUri);
+  }
+
+  return c.json({ startingLogin: providerName });
+});
+
+app.get("/api/profile", async (c) => {
+  const token = getCookie(c, "token")!;
+
+  const { userinfo_endpoint } = await getDiscoveryDoc();
+
+  console.log({ userinfo_endpoint, token });
+
+  const res = await fetch(userinfo_endpoint, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  res.ok;
+  return c.json(await res.json());
+});
 
 app.get("*", serveStatic({ root: "../dist" }));
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,65 @@
 import { createRoot } from "react-dom/client";
+import {
+  BrowserRouter,
+  HashRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-function Application() {
-  return <h1>Hello World</h1>;
+function FrontPage() {
+  return (
+    <>
+      <h1>Front page</h1>
+      <ul>
+        <li>
+          <Link to={"/profile"}>User profile</Link>
+        </li>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
-createRoot(document.getElementById("app")!).render(<Application />);
+function LoginPage() {
+  return (
+    <>
+      <h1>Please log in</h1>
+      <ul>
+        <li>
+          <li>
+            <a href="/api/login/microsoft/start">log in with Microsoft</a>
+          </li>
+          <li>
+            <a href="/api/login/google/start">log in with Google</a>
+          </li>
+          <li>
+            <a href="/api/login/github/start">log in with GitHub</a>
+          </li>
+          <li>
+            <a href="/api/login/linkedin/start">log in with LinkedIn</a>
+          </li>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+function Application() {
+  return (
+    <Routes>
+      <Route path={"/"} element={<FrontPage />} />
+      <Route path={"/profile"} element={<h1>User info goes here</h1>} />
+      <Route path={"/login"} element={<LoginPage />} />
+      <Route path={"*"} element={<h1>Not found in frontend</h1>} />
+    </Routes>
+  );
+}
+
+createRoot(document.getElementById("app")!).render(
+  <BrowserRouter>
+    <Application />
+  </BrowserRouter>,
+);
