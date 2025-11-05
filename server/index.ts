3e1cf52baeac7b543fb10b7cdfcdc360bd0f2bb3
Johannes Brodwall <jhannes@gmail.com>
2025-11-05 12:59:44 +0100

    fetching data from MongoDb

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
 import type { RentalLocation } from "../shared/rentalLocation.js";
+import { MongoClient } from "mongodb";
 
 const app = new Hono();
 
@@ -10,8 +11,17 @@ serve({ fetch: app.fetch, port: parseInt(port) });
 
 app.get("*", serveStatic({ root: "../dist" }));
 
-const locations: RentalLocation[] = [
-  { _id: "1", name: "Server apartment", summary: "Nice" },
-  { _id: "2", name: "Server cabin", summary: "Beautiful" },
-];
-app.get("/api/locations", (c) => c.json(locations));
+const client = new MongoClient(process.env.MONGODB_URL!);
+const connection = await client.connect();
+const collection = connection
+  .db("sample_airbnb")
+  .collection<RentalLocation>("listingsAndReviews");
+
+app.get("/api/locations", async (c) =>
+  c.json(
+    await collection
+      .find({ property_type: "House", "address.market": "Porto" })
+      .limit(200)
+      .toArray(),
+  ),
+);
